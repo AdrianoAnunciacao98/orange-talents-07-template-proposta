5edea768fdcfdbe5d566efa71fa5f215d72d5fe7
= <=>
2021-08-16 10:27:07 -0300

    Setup do projeto

---
+++ src/test/java/br/com/zupacademy/adriano/microservicepropostas/MicroservicePropostasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.adriano.microservicepropostas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicePropostasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
