b62dff9af56ee3c9d769c0add625f142ed9c75a9
= <=>
2021-08-24 15:00:55 -0300

    Endpoint com métricas

--- src/main/java/br/com/zupacademy/adriano/microservicepropostas/config/SecurityConfiguration.java
+++ src/main/java/br/com/zupacademy/adriano/microservicepropostas/config/SecurityConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
+    private static final String ACTUATOR= "/actuator/**";
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorizeRequests ->
@@ -16,6 +16,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .antMatchers(HttpMethod.POST, "/cartoes/**").hasAuthority("SCOPE_cartoes:write")
                         .antMatchers(HttpMethod.POST, "/propostas/**").hasAuthority("SCOPE_propostas:write")
                         .antMatchers(HttpMethod.POST, "/biometria").hasAuthority("SCOPE_biometria:write")
+                        .antMatchers(HttpMethod.GET, ACTUATOR).hasAuthority("SCOPE_propostas:read")
                         .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
