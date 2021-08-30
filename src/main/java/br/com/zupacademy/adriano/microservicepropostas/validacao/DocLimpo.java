1c6cef01990fc2c45c869762801ebce344ce76cd
= <=>
2021-08-30 10:19:05 -0300

    Documento limpo

---
+++ src/main/java/br/com/zupacademy/adriano/microservicepropostas/validacao/DocLimpo.java
@@ -0,0 +1,21 @@
+package br.com.zupacademy.adriano.microservicepropostas.validacao;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
+public class DocLimpo {
+
+        private String doc;
+
+        @Valid
+        public DocLimpo(@NotBlank String doc) {
+            this.doc = doc;
+        }
+
+        public String documentoEncriptado() {
+            return new BCryptPasswordEncoder().encode(doc);
+        }
+    }
+}
